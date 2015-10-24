a341ebf6addc02caf6753b35c47b543c70971fdc
Brunno dos Santos <squiter85@gmail.com>
2015-10-24 15:06:36 -0200

    Initial Commit

---
+++ maps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  maps
+//
+//  Created by Treinamento Mobile on 10/24/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ maps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  maps
+//
+//  Created by Treinamento Mobile on 10/24/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
